1e6c0655fda9c710b756bd97783714152fff9690
shuya0070602 <shuya.070602@gmail.com>
2020-03-08 17:03:11 +0900

    six

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_014227) do
+ActiveRecord::Schema.define(version: 2020_03_08_075445) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
