226aa5ff6058d431ea86ad0af64b9b011b14739c
shuya0070602 <shuya.070602@gmail.com>
2020-03-10 15:53:46 +0900

    seven

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 2020_03_08_075445) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_image_id"
     t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
